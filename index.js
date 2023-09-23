51dff72036df5076d8629d550dfe941d4f1b4a4a
U-2PTKRL2C5504U\Martin Bartoš <martin.bartos@behaviolabs.com>
2023-09-23 15:06:09 +0200

    fresh start

--- index.js
+++
@@ -1,1 +0,0 @@
-console.log("mea first line")
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>MEANDERING</title>
+      </Head>
+      <div>
+        <h1>MEANDERING</h1>
+      </div>
+    </>
+  );
+}
